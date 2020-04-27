f0cff8ef654b6c922a5cd2ec26fd5e53e11c0d0a
Feida <feidameng01@gmail.com>
2020-04-27 02:48:02 -0400

    changed url to test getCasesByCityAndDate router

--- router/covid19Router.js
+++ router/covid19Router.js
@@ -5,7 +5,7 @@ const { getLatestCases, getAllCases, getCasesByCityAndDate } = require('../contr
 
 router.route('/history').get(getAllCases);
 
-router.route('/getcasesbycitydate').get(getCasesByCityAndDate);
+router.route('/chartdata').get(getCasesByCityAndDate);
 
 router.route('/').get(getLatestCases);
 
